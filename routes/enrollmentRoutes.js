ea642a25986a3d8aa191ab41d40ccea5e23ccf84
PrimaryKey <skokomi396@gmail.com>
2026-02-06 02:42:01 +0500

    Add course stats and student course stats routes
    Added new routes for course statistics and student course stats.

--- routes/enrollmentRoutes.js
+++ routes/enrollmentRoutes.js
@@ -8,12 +8,13 @@ const { protect, adminOnly, superAdminOnly } = require('../middleware/auth');
 router.get('/my-courses', protect, enrollmentController.getMyCourses);
 router.get('/course/:courseId', protect, enrollmentController.getCourseDetails);
 router.get('/stats', protect, enrollmentController.getGPA);
+router.get('/course/:courseId/stats', protect, adminOnly, enrollmentController.getCourseStats);
 
 
 router.get('/course/:courseId/students', protect, adminOnly, enrollmentController.getStudentsInCourse);
 router.post('/grade', protect, adminOnly, enrollmentController.submitGrade);
 router.post('/assign', protect, adminOnly, enrollmentController.assign);
-
+router.get('/course/:courseId/my-stats', protect, enrollmentController.getStudentCourseStats);
 router.get('/manage/all', protect, superAdminOnly, enrollmentController.getAllEnrollments);
 router.delete('/manage/:id', protect, superAdminOnly, enrollmentController.unenroll);
 module.exports = router;
